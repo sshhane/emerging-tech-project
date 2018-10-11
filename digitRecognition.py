313179bb203bc39f0966762c037a768e96df4328
sshhane <shaned257@gmail.com>
2018-10-11 15:13:03 +0100

    added digit script

---
+++ digitRecognition.py
@@ -0,0 +1,3 @@
+# 4. Digit recognition script: 
+# a Python script that takes an image file containing a handwritten digit and identifies
+# the digit using a super- vised learning algorithm and the MNIST dataset.

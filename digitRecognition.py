1f39aa30fe1b680793d6f283c73f67b682e5c574
sshhane <shaned257@gmail.com>
2018-11-28 19:13:59 +0000

    digit recognition, not working

--- digitRecognition.py
+++ digitRecognition.py
@@ -1,5 +1,18 @@
 import numpy
 from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+
+
+from keras.utils import np_utils
+
+
+# from keras.utils import np_utils
 
 # load data
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
@@ -7,5 +20,35 @@ from keras.datasets import mnist
 X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
 X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
 
-# def base_model():
+# one hot encode outputs
+y_train = np_utils.to_categorical(y_train)
+y_test = np_utils.to_categorical(y_test)
+num_classes = y_test.shape[1]
+
+def base_model():
     # create model
+    model = Sequential()
+    model.add(Conv2D(32, (5, 5), input_shape=(1, 28, 28), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    # Compile model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	# Compile model
+
+    # compile
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+# build model
+model = base_model()
+
+# Fit model
+# epoch = iteration over all data, batch size = no. of images at a time
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=200, verbose=2)
+
+# eval
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("CNN Error: %.2f%%" % (100-scores[1]*100))

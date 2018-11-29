dc6f6461c602607e18e03088db4a9f488f68b43d
sshhane <shaned257@gmail.com>
2018-11-29 15:34:40 +0000

    added argparse

--- digitRecognition.py
+++ digitRecognition.py
@@ -1,3 +1,10 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--train", help="Train a model on MNIST data",  action='store_true')
+parser.add_argument("-r", "--recognise", help="Recognise a given digit")
+args = parser.parse_args()
+
 import numpy
 from keras.datasets import mnist
 from keras.models import Sequential
@@ -8,27 +15,40 @@ from keras.layers import Flatten
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 
-
 from keras.utils import np_utils
+
+# save / load model
+from keras.models import load_model
 
 # convolutional neural network (cnn)
 from keras import backend as K
 K.set_image_dim_ordering('th')
 
-# load data
-(X_train, y_train), (X_test, y_test) = mnist.load_data()
-# reshape to be [samples][pixels][width][height]
-X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
-X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+if args.recognise:
+    print("Selected rec")
 
-# normalize inputs from 0-255 to 0-1
-X_train = X_train / 255
-X_test = X_test / 255
+    print(args.recognise)
 
-# one hot encode output variables
-y_train = np_utils.to_categorical(y_train)
-y_test = np_utils.to_categorical(y_test)
-num_classes = y_test.shape[1]
+# option -t
+if args.train:
+    print("Selected train")
+    # load data
+    (X_train, y_train), (X_test, y_test) = mnist.load_data()
+    # reshape to be [samples][pixels][width][height]
+    X_train = X_train.reshape(X_train.shape[0], 1, 28, 28).astype('float32')
+    X_test = X_test.reshape(X_test.shape[0], 1, 28, 28).astype('float32')
+
+    # normalize inputs from 0-255 to 0-1
+    X_train = X_train / 255
+    X_test = X_test / 255
+
+    # one hot encode output variables
+    y_train = np_utils.to_categorical(y_train)
+    y_test = np_utils.to_categorical(y_test)
+    num_classes = y_test.shape[1]
+
+    # train model
+    train_model()
 
 def base_model():
     # create model
@@ -44,13 +64,17 @@ def base_model():
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     return model
 
-# build model
-model = base_model()
+def train_model():
+    # build model
+    model = base_model()
 
-# Fit model
-# epoch = iteration over all data, batch size = no. of images at a time
-model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
+    # Fit model
+    # epoch = iteration over all data, batch size = no. of images at a time
+    model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200, verbose=2)
 
-# eval
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("CNN Error: %.2f%%" % (100-scores[1]*100))
+    # save model
+    model.save('model.h5')
+
+    # eval
+    scores = model.evaluate(X_test, y_test, verbose=0)
+    print("CNN Error: %.2f%%" % (100-scores[1]*100))

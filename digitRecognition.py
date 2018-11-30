b0918ab3e249485142cf09d3f796f94b618836c7
sshhane <shaned257@gmail.com>
2018-11-30 17:18:37 +0000

    added testing on new image

--- digitRecognition.py
+++ digitRecognition.py
@@ -5,30 +5,61 @@ parser.add_argument("-t", "--train", help="Train a model on MNIST data",  action
 parser.add_argument("-r", "--recognise", help="Recognise a given digit")
 args = parser.parse_args()
 
-import numpy
+import numpy as np
+import keras
 from keras.datasets import mnist
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Dropout
 from keras.layers import Flatten
 
+# from keras.datasets import mnist
+# from keras.layers import Dense
+# from keras.models import Sequential
+# from keras.optimizers import SGD
+import matplotlib.pyplot as plt
+
 from keras.layers.convolutional import Conv2D
 from keras.layers.convolutional import MaxPooling2D
 
 from keras.utils import np_utils
+
+from keras.preprocessing import image
 
 # save / load model
 from keras.models import load_model
 
 # convolutional neural network (cnn)
 from keras import backend as K
-
 K.set_image_dim_ordering('th')
 
 if args.recognise:
     print("Selected rec")
     filename = args.recognise
+
+    img = image.load_img(path="mnist.png",color_mode = "grayscale",target_size=(28,28,1))
+    img = image.img_to_array(img)
+
+    test_img = img.reshape((1,784))
+
+    test_img = np.expand_dims(img, axis=0)
+
     print(filename)
+
+    model = keras.models.load_model('model.h5')
+
+    # model.predict_classes()
+
+    img_class = model.predict_classes(test_img)
+    prediction = img_class[0]
+
+    classname = img_class[0]
+
+    print("Class: ",classname)
+    img = img.reshape((28,28))
+    plt.imshow(img)
+    plt.title(classname)
+    plt.show()
 
 # option -t
 if args.train:
